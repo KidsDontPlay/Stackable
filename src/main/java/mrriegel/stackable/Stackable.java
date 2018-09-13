a993384065a9eb923ad0ba9c2c6dc6918418b5ca
gaudus <ichhabads@gmail.com>
2018-09-13 19:36:58 +0200

    build 1.3.1

--- src/main/java/mrriegel/stackable/Stackable.java
+++ src/main/java/mrriegel/stackable/Stackable.java
@@ -58,7 +58,7 @@ public class Stackable {
 	@Instance(Stackable.MODID)
 	public static Stackable INSTANCE;
 
-	public static final String VERSION = "1.3.0";
+	public static final String VERSION = "1.3.1";
 	public static final String NAME = "Stackable";
 	public static final String MODID = "stackable";
 
